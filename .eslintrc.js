75e7a052e2303af36b65348559ab13b892e29c1e
IBlancI <adilslimani93130@gmail.com>
2024-07-19 20:45:42 +0200

    test

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    env: {
+      es6: true,
+      node: true,
+      jest: true,
+    },
+    extends: ['eslint:recommended'],
+    parserOptions: {
+      ecmaVersion: 2021,
+    },
+    rules: {
+      // Add specific rules here if needed
+    },
+  };
+  
