76325801f181639b014bcc33271fd8debc4aae56
IBlancI <adilslimani93130@gmail.com>
2024-07-26 21:45:32 +0200

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,17 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName), // Call signUpUser with firstName and lastName
+    uploadPhoto(fileName) // Call uploadPhoto with fileName
+  ]);
+
+  // Map results to the required format
+  return results.map(result => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+    value: result.status === 'fulfilled' ? result.value : result.reason
+  }));
 }
+
+export default handleProfileSignup;
