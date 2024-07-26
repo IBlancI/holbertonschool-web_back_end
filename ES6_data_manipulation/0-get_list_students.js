1f3f9b14d2e8b6fcadc8bee6ea0e1e06a533f76e
IBlancI <adilslimani93130@gmail.com>
2024-07-26 21:23:58 +0200

    test to fix

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,12 @@
-export default function getListStudents() {
-  const array = [
+function getListStudents() {
+  // Create an array of student objects
+  const students = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
-  return array;
+  
+  return students; // Return the array
 }
+
+export default getListStudents; // Export the function
