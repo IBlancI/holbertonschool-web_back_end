2fc0a22a1326fa89aa6a3542b56e9d188075d68a
IBlancI <adilslimani93130@gmail.com>
2024-08-05 23:48:21 +0200

    test

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -4,10 +4,10 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
+    getIncomeInDollars(income) { // Méthode ES6
       return `$${income}`;
     },
-    getIncomeInEuros(income) {
+    getIncomeInEuros(income) { // Méthode ES6
       return `${income} euros`;
     },
   };
